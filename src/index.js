f12641f8a679b548c42ba0da94f38016cae62356
ElhamSoldouzi <134371922+ElhamSoldouzi@users.noreply.github.com>
2024-12-06 22:12:31 +0330

    add a line to ls

--- src/index.js
+++ src/index.js
@@ -58,3 +58,5 @@ function searchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
+
+searchCity("Urmia");
